c16bf8bcd9ef0fbf727858e6492955e18ace2c2f
rafiul-2001 <rafiulislam2001@gmail.com>
2025-04-12 20:11:48 +0600

    Web Storage Api

---
+++ web storage api/readME.js
@@ -0,0 +1,32 @@
+//how to see local storage via inspect in the website: inspect->application->localstorage
+// key: setItem,getItem, removeItem
+
+
+
+
+//setItem
+// localStorage.setItem() take key value pairs and store also both must be string
+localStorage.setItem("Name", "Rafiul");
+localStorage.setItem("ID", "011221086");
+
+// localStorage cannot take value other than string. So if any array is passed. it need to convert into Json file that is "JSON.stringify(data)";
+const language = ["bangla", "english"];
+localStorage.setItem("Language", JSON.stringify(language));
+
+
+
+
+//getItem
+const name = localStorage.getItem("Name");
+console.log(name);
+//array need to convert to JSON to javascript
+const x = JSON.parse(localStorage.getItem("Language"));
+console.log(x);
+
+
+
+
+//removeItem
+localStorage.removeItem("ID");
+
+
